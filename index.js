0156c429cdedd98279437c3b4fb224fb37a38790
jvariara <jvariara@gmail.com>
2022-07-24 19:55:35 -0400

    Finished the premise of eportfolio

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 // template_m6krp3a
 // service_zr7d8kh
 // q89ZcCUF1ZCYFxvu6
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -10,10 +34,10 @@ function contact(event) {
 
   emailjs
     .sendForm(
-      'service_zr7d8kh',
-      'template_m6krp3a',
+      "service_zr7d8kh",
+      "template_m6krp3a",
       event.target,
-      'q89ZcCUF1ZCYFxvu6'
+      "q89ZcCUF1ZCYFxvu6"
     )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
@@ -25,4 +49,14 @@ function contact(event) {
         "The email service is temporarily unavailable. Please contact me directly at jvariara@gmail.com"
       );
     });
+}
+
+function toggleModal() {
+  // toggle modal
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
